584f5e1550963719cdcdff4aa3298352a3dfc5a3
mihaimusteata <mihail.musteata@isa.utm.md>
2026-02-09 17:46:09 +0200

    add theme and i18n providers with context hooks and initial configuration

--- MIBO.Client/client/src/app/App.tsx
+++ MIBO.Client/client/src/app/App.tsx
@@ -1,11 +1,12 @@
-import ChatPage from "./pages/ChatPage.tsx";
+import { ThemeProvider } from "./providers/ThemeProvider";
+import { I18nProvider } from "./providers/I18nProvider";
 
-function App() {
-  return (
-    <>
-        <ChatPage />
-    </>
-  )
+export default function App() {
+    return (
+        <ThemeProvider>
+            <I18nProvider>
+                application
+            </I18nProvider>
+        </ThemeProvider>
+    );
 }
-
-export default App
---
+++ MIBO.Client/client/src/theme/tokens.ts
@@ -0,0 +1,2 @@
+export const themeKey = "theme";
+export type ThemeMode = "dark" | "light";
