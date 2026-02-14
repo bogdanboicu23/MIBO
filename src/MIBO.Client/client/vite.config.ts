c144988f65530e2246576ded2933704874ba056b
mihaimusteata <mihail.musteata@isa.utm.md>
2026-02-15 00:37:32 +0200

    Update ESLint configuration and project structure
    - Switched import paths to use `@` alias.
    - Added and disabled multiple ESLint rules.
    - Introduced `vite-plugin-checker` for TypeScript and ESLint during development.
    - Updated `package-lock.json` with new dependencies and refined configurations.
    - Adjusted `vite.config.ts` with new resolver aliases and plugins.

--- src/MIBO.Client/client/vite.config.ts
+++ src/MIBO.Client/client/vite.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
+import checker from 'vite-plugin-checker';
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        tailwindcss()
+        tailwindcss(),
+        checker({
+            typescript: true,
+            eslint: {
+                useFlatConfig: true,
+                lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+                dev: { logLevel: ['error'] },
+            },
+            overlay: {
+                position: 'tl',
+                initialIsOpen: false,
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(process.cwd(), 'src'),
+            src: path.resolve(process.cwd(), 'src'),
+        },
+    },
 })
