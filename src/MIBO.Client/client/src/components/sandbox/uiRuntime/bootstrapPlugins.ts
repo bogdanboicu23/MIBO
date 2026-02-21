cef1804479226c7b069ad98d4f993965f3b0eaf8
mihaimusteata <mihail.musteata@isa.utm.md>
2026-02-21 20:33:01 +0200

    Remove expenses feature and related modules
    - Delete all code, types, UI components, and mock data associated with the expenses feature.
    - Remove plugin registration (`expensesDashboardPlugin`) and dependencies related to expenses from the `uiRuntime` module.

--- src/MIBO.Client/client/src/components/sandbox/uiRuntime/bootstrapPlugins.ts
+++ src/MIBO.Client/client/src/components/sandbox/uiRuntime/bootstrapPlugins.ts
@@ -1,6 +1,4 @@
-import { registerPlugin } from "./Registry";
-import { expensesDashboardPlugin } from "../../../features/expenses/expenses.plugin.ts";
 
 export function bootstrapPlugins() {
-    registerPlugin(expensesDashboardPlugin);
+    // registerPlugin(expensesDashboardPlugin);
 }
--- src/MIBO.Client/client/src/components/sandbox/uiRuntime/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./specTypes";
-export * from "./specValidation";
-export * from "./specNormalizer";
-export * from "./UiPlugin";
-export * from "./Registry";
-export * from "./UiRenderer";
